277834c31603782098be090955205e79941a7202
Accel <denisignatenko2006@gmail.com>
2025-10-06 14:43:57 +0700

    Добавьте файлы проекта.

---
+++ ToDo.Core.Tests/ToDoListTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ToDo.Core;
+
+namespace ToDo.Core.Tests
+{
+    public class ToDoListTests
+    {
+        [Fact]
+        public void Add_IncreasesCount()
+        {
+            var list = new ToDoList();
+            list.Add(" task ");
+            Assert.Equal(1, list.Count);
+            Assert.Equal("task", list.Items.First().Title);
+        }
+
+        [Fact]
+        public void Remove_ById_Works()
+        {
+            var list = new ToDoList();
+            var item = list.Add("a");
+            Assert.True(list.Remove(item.Id));
+            Assert.Equal(0, list.Count);
+        }
+
+        [Fact]
+        public void Find_ReturnsMatches()
+        {
+            var list = new ToDoList();
+            list.Add("Buy milk");
+            list.Add("Read book");
+            var found = list.Find("buy").ToList();
+            Assert.Single(found);
+            Assert.Equal("Buy milk", found[0].Title);
+        }
+    }
+}
---
+++ ToDo.Core/ToDoList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo.Core
+{
+    public class ToDoList
+    {
+        private readonly List<ToDoItem> _items = new();
+
+        public IReadOnlyList<ToDoItem> Items => _items.AsReadOnly();
+
+        public ToDoItem Add(string title)
+        {
+            var item = new ToDoItem(title);
+            _items.Add(item);
+            return item;
+        }
+
+        public bool Remove(Guid id) => _items.RemoveAll(i => i.Id == id) > 0;
+
+        public IEnumerable<ToDoItem> Find(string substring) => _items.Where(i => i.Title.Contains(substring ?? string.Empty, StringComparison.OrdinalIgnoreCase));
+
+        public int Count => _items.Count;
+    }
+}
